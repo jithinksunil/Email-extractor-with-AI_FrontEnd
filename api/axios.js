4c2cb1edf560e02712dd73677303d0e5133cc3bd
jithinksunil <jithinksunil96@gmail.com>
2024-03-05 00:45:16 +0530

    updates

--- api/axios.js
+++ api/axios.js
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
 export const axiosPublic = axios.create({
-  baseURL: "http://localhost:8000/gauth",
+  baseURL: 'http://localhost:8000',
 });
 
 export const axiosPrivate = axios.create({
-  baseURL: "http://localhost:8000/gauth",
+  baseURL: 'http://localhost:8000',
   headers: { 'Content-Type': 'application/json' },
 });
